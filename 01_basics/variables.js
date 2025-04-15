5760ec0925a718964664f9278df1c0bde32e673b
itskeshavsingh <145543209+itskeshavsingh@users.noreply.github.com>
2025-04-15 22:24:41 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+const accountId=12345
+let accountEmail="keshav@gmail.com"
+var accountPass="23456"
+accountCity="jaipur"
+
+// accountId=2
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity])
