d9779a0079fd92747f1b63775c7bbe2ba0523946
itskeshavsingh <145543209+itskeshavsingh@users.noreply.github.com>
2025-04-19 20:36:04 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,14 @@
+const gameName ="keshavsh"
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('v'));
+
+const newString = gameName.slice(-8,4);
+console.log(newString);
+
+const str="  keshav   "
+console.log(str.trim());
+
+const url="https://symmetrical-cod-q7q5467vpp4x2rr4.github.dev/";
+console.log(url.replace('cod','-'));
+
+console.log(`my in game name is ${gameName} and my real name is ${str}`);
