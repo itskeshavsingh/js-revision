89cafaa1763031350eaea65d847b6bbfff1e3232
itskeshavsingh <145543209+itskeshavsingh@users.noreply.github.com>
2025-04-19 22:33:03 +0000

    date

---
+++ 01_basics/date.js
@@ -0,0 +1,7 @@
+let myDate=new Date()
+console.log(myDate.toDateString()); 
+console.log(myDate.toISOString()); 
+console.log(myDate.toLocaleDateString()); 
+// console.log(myDate.toISODateString()); 
+let timeStamp= Date.now();
+console.log(timeStamp);
