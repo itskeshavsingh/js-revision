e6b0813e1909941a3405dafd461bc5aea4bc49d9
itskeshavsingh <145543209+itskeshavsingh@users.noreply.github.com>
2025-04-19 23:08:18 +0000

    array

---
+++ 02-basics/array.js
@@ -0,0 +1,10 @@
+console.log('Hello from array.js');
+const myArr=[1,2,3,3]
+console.log((myArr[0]));
+myArr.push(6)
+console.log(myArr);
+myArr.pop();
+myArr.unshift(7)
+myArr.shift()
+
+console.log(myArr);
