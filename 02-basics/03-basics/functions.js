8db2c6da4a2120fafec868c089f9a2f703cc80e2
itskeshavsingh <145543209+itskeshavsingh@users.noreply.github.com>
2025-05-13 07:16:12 +0000

    functions basics

---
+++ 02-basics/03-basics/functions.js
@@ -0,0 +1,10 @@
+function addition(number1,number2){
+    console.log(number1 + number2);
+}
+
+  addition(3,4)
+
+  function calculateCartPrice(...num1){
+    return num1
+  }
+  console.log(calculateCartPrice(200,400,500,2348))
