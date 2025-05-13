54c899d50134764cbeecd9e77e5d7c481b1170df
itskeshavsingh <145543209+itskeshavsingh@users.noreply.github.com>
2025-05-13 12:09:52 +0000

    arrow func

--- 02-basics/03-basics/functions.js
+++ 02-basics/03-basics/functions.js
@@ -7,4 +7,16 @@ function addition(number1,number2){
   function calculateCartPrice(...num1){
     return num1
   }
-  console.log(calculateCartPrice(200,400,500,2348))
+  // console.log(calculateCartPrice(200,400,500,2348))
+
+  // let a=10
+  // const b=20
+  // var c=30
+  
+  // const chai = () => {
+  //   let username = "keshav"
+  //   console.log(this.username)
+  // }
+  //  chai()
+
+     
