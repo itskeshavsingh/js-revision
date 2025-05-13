596fb0aef1233709d30aea70f5d5991e26e12f1b
itskeshavsingh <145543209+itskeshavsingh@users.noreply.github.com>
2025-05-13 06:35:00 +0000

    object

---
+++ 02-basics/objects.js
@@ -0,0 +1,13 @@
+const JsUser ={
+    name: "Keshav"
+}
+const tinderuser = {
+
+    Id : "123abc",
+    name : "sammy",
+   isloggedIn:"false" }
+
+    console.log(tinderuser);
+    console.log(Object.keys(tinderuser));
+    console.log(Object.values(tinderuser));
+    
