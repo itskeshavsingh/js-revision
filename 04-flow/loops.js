d582a4888970c383f8a0a891c8b3c25ad1abc60c
itskeshavsingh <145543209+itskeshavsingh@users.noreply.github.com>
2025-05-14 11:15:49 +0000

    loops map

---
+++ 04-flow/loops.js
@@ -0,0 +1,19 @@
+ const Array=[1,2,3,4,6];
+ for(let i=0;i<=Array.length;i++){
+   const element=i;
+   console.log(i);
+}
+// for of loop
+const greetings = "Hello World"
+for(const greet of greetings){
+  console.log(`each char is ${greet}`)
+}
+// maps in js
+const map = new Map ()
+
+map.set('IN',"INDIA")
+map.set('FR',"FRANCE")
+
+console.log(map);
+
+ 
